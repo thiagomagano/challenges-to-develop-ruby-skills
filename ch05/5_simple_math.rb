2beca97d6a46fa72a9451c25bede330924947d95
Thiago Magano <thiagomagano1993@outlook.com>
2025-10-23 15:27:23 -0300

    challenge 05 done

--- ch05/5_simple_math.rb
+++ ch05/5_simple_math.rb
@@ -1,13 +1,40 @@
 #!/home/thi/.rbenv/versions/3.4.7/bin/ruby
 
-# Basic function
-def greet(name)
-  "Hello #{name}, nice to meet you!"
+
+def add(a, b)
+  a + b
+end
+def sub(a, b)
+  a - b
+end
+def multi(a, b)
+  a * b
+end
+def div(a, b)
+  a / b
 end
 
-# Basic program template
+def simple_math(a, b)
+  str_add = "#{a} + #{b} = #{add(a,b)}\n"
+  str_sub = "#{a} - #{b} = #{sub(a,b)}\n"
+  str_multi = "#{a} * #{b} = #{multi(a,b)}\n"
+  str_div = "#{a} / #{b} = #{div(a,b)}"
+
+  return str_add + str_sub + str_multi + str_div
+end
+
 if __FILE__ == $0
-  print 'Qual é o seu nome? '
-  name = gets.chomp
-  puts greet(name)
+  print 'Qual é o primeiro número? '
+  num_a = gets.chomp
+  print 'Qual é o segundo número? '
+  num_b = gets.chomp
+
+  if num_a.match?(/^\d+$/) && num_b.match?(/^\d+$/)
+    a = num_a.to_i
+    b = num_b.to_i
+    puts simple_math(a,b)
+  else
+    puts "Você precisa digitar dois números validos"
+  end
+
 end
--- ch05/5_simple_math_test.rb
+++ ch05/5_simple_math_test.rb
@@ -1,13 +1,33 @@
-# Base test file
 require 'minitest/autorun'
 
-# TODO: Add a require_relative to your main challenge file
-# e.g., require_relative '1_my_challenge'
+require_relative '5_simple_math'
 
 class ChallengeTest < Minitest::Test
-  def test_example
-    want = "" # Valor esperado
-    got= "" # Chamada pra função
+  def test_add
+    want = 12
+    got= add(10,2)
     assert_equal want, got
+  end
+  def test_sub
+    want = 8
+    got= sub(10,2)
+    assert_equal want, got
+  end
+  def test_multi
+    want = 20
+    got= multi(10,2)
+    assert_equal want, got
+  end
+  def test_div
+    want =  5
+    got= div(10,2)
+    assert_equal want, got
+  end
+
+  def test_simple_math
+    want = "10 + 2 = 12\n10 - 2 = 8\n10 * 2 = 20\n10 / 2 = 5"
+    got = simple_math(10,2)
+
+    assert_match want, got
   end
 end
